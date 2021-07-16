b164c2fdf57d312f5cab7b5d655fbc5f04b367db
SDET-Krunal <87511691+SDET-Krunal@users.noreply.github.com>
2021-07-16 09:46:00 +0530

    Program to check the anagram string or not

---
+++ String Programs/AnagramString.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class AnagramString {
+    public static void main(String args[]) {
+        String s1 = "silent";  // e.g: Keep, MotherInLaw
+	    	String s2 = "listen"; // e.g: Peek, HitlerWoman
+
+				if IsStringAnagram(s1, s2){
+					System.out.println("Both strings " + s1 + " and " + s2 + " are Anagram.");
+				}
+				else{
+					System.out.println("Both strings are not Anagram.");
+				}
+    }
+    
+    public static Boolean IsStringAnagram(String s1, String s2){
+			if s1.length() != s2.length(){
+				return false;
+			}
+			else{
+				char[] as1 = s1.toLowerCase().toCharArray();
+				char[] as2 = s2.toLowerCase().toCharArray();
+
+				Arrays.sort(as1);
+				Arrays.sort(as2);
+
+				return Arrays.equals(ArrayS1, ArrayS2);
+			}
+    }
+}
