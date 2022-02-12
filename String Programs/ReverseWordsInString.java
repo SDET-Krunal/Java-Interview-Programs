87f5f8e2b43e055834d6b6f017b427b773dc3756
Krunal Panchal <87511691+SDET-Krunal@users.noreply.github.com>
2022-02-12 09:56:09 +0530

    Update ReverseWordsInString.java

--- String Programs/ReverseWordsInString.java
+++ String Programs/ReverseWordsInString.java
@@ -1,19 +1,19 @@
 public class ReverseWordsInString
 {
 	public static void main(String[] args) {
-	    String s = "Automation Testing";
-        String reverse = "";
-        
-        for(String str: s.split(" ")){
-            String reverse_word = "";
-            
-        	for(int i=str.length()-1; i>=0; i--){
-        		reverse+=str.charAt(i);
-        	}
-        	
-        	reverse = reverse + reverse_word + " ";
-        }
-        
-        System.out.println(reverse);
+	    	String s = "Automation Testing";
+		String reverse = "";
+
+		for(String str: s.split(" ")){
+		    String reverse_word = "";
+
+			for(int i=str.length()-1; i>=0; i--){
+				reverse+=str.charAt(i);
+			}
+
+			reverse = reverse + reverse_word + " ";
+		}
+
+		System.out.println(reverse);
 	}
 }
